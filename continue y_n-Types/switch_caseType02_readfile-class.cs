a5ba8702d3ae7b089a101adee290d030ecf6c978
zhaotan18x <61022230+zhaotan18x@users.noreply.github.com>
2020-12-28 21:05:56 +0200

    pääohjelma lukee tiedoston luokka1, lukaista uuestaan y/n

---
+++ continue y_n-Types/switch_caseType02_readfile-class.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Class1
+  {
+    public void Tervehdi()
+      {
+            Console.WriteLine("Hello, Earth");
+      }
+
+    public string MuuTervehdys()
+    {
+      return "Case Closed";
+    }
+       
+  }
