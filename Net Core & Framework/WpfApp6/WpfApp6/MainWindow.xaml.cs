5582ed87f231a921674e0dcbb9419eff693a3c89
zhaotan18x <61022230+zhaotan18x@users.noreply.github.com>
2020-12-28 13:16:31 +0200

    Add files via upload

---
+++ Net Core & Framework/WpfApp6/WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuItem_Click_1(object sender, RoutedEventArgs e)
+        {
+        Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+            ofd.ShowDialog();
+            Title = System.IO.Path.GetFileName(ofd.FileName);
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            TextRange textRange
+                //= new TextRange(RichTextBox.Document.ContentStart, RichTextBox.Document.ContentEnd);
+                = new TextRange(RichTextBox.Selection.Start, RichTextBox.Selection.End);
+
+            TextBlock.Inlines.Clear();
+            TextBlock.Inlines.Add("Eka rivi\n");
+            TextBlock.Inlines.Add(new Run(textRange.Text) { Foreground = Brushes.Blue });
+            TextBlock.Inlines.Add("Viimeinen rivi");
+        }
+    }
+}
