92ab8726b3e811774b76e6088ae88eff58d26b62
zhaotan18x <61022230+zhaotan18x@users.noreply.github.com>
2020-12-29 11:17:45 +0200

    Pelaaja class

---
+++ Class/Get-Set/Type02/Pelaaja.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp18
+{
+    class Pelaaja
+    {
+        string alias;
+        int pisteet;
+
+        //ctor <tab><tab>
+        public Pelaaja()
+        {
+
+        }
+
+        public string Alias { get => alias; set => alias = value; }
+        public int Pisteet { get => pisteet; set => pisteet = value; }
+    }
+}
